d6aab59812710ebcd7c298bff034a6ad9dc223d0
arsalmahmood1998 <68080848+arsalmahmood1998@users.noreply.github.com>
2020-11-03 22:39:05 +0500

    code updation

--- CSVReader.py
+++ CSVReader.py
@@ -1,11 +1,15 @@
 def openFile(fileName):
-    with open(fileName) as file:
-        listOflines=list(file)
-        line=listOflines[0]
-        colHeaders = line.split(",")
-        rawRows = listOflines[1:]
-        rows=[]
-        for line in rawRows:
-            row=line.split(",")
-            rows.append(row)
-    return colHeaders,rows
+    try:
+        with open(fileName) as file:
+                listOflines=list(file)
+                line=listOflines[0]
+                colHeaders = line.split(",")
+                rawRows = listOflines[1:]
+                rows=[]
+                for line in rawRows:
+                    row=line.split(",")
+                    rows.append(row)
+                return colHeaders,rows
+    except FileNotFoundError:
+           print("File does not exists")
+           exit()
